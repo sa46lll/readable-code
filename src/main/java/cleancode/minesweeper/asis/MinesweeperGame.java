89872d48e7bc326f64e0bedeb08d999aa5f51b52
서명현 <azure394@gmail.com>
2024-08-18 21:49:35 +0900

    refactor: cell 관심사 분리

--- src/main/java/cleancode/minesweeper/asis/MinesweeperGame.java
+++ src/main/java/cleancode/minesweeper/asis/MinesweeperGame.java
@@ -16,10 +16,6 @@ public class MinesweeperGame {
     private static final Cell[][] BOARD = new Cell[BOARD_ROW_SIZE][BOARD_COL_SIZE];
     private static final Integer[][] NEARBY_LAND_MINE_COUNT = new Integer[BOARD_ROW_SIZE][BOARD_COL_SIZE];
     private static final boolean[][] LAND_MINES = new boolean[BOARD_ROW_SIZE][BOARD_COL_SIZE];
-    public static final String FLAG_SIGN = "⚑";
-    public static final String CLOSED_CELL_SIGN = "□";
-    public static final String LAND_MINE_SIGN = "☼";
-    public static final String OPENED_CELL_SIGN = "■";
 
     private static int gameStatus = 0; // 0: 게임 중, 1: 승리, -1: 패배
 
@@ -56,14 +52,14 @@ public class MinesweeperGame {
         int selectedRowIndex = getSelectedRowIndex(cellInput);
 
         if (doesUserChooseToPlantFlag(userActionInput)) {
-            BOARD[selectedRowIndex][selectedColIndex] = Cell.of(FLAG_SIGN);
+            BOARD[selectedRowIndex][selectedColIndex] = Cell.ofFlag();
             checkIfGameIsOver();
             return;
         }
 
         if (doesUserChooseToOpenCell(userActionInput)) {
             if (isLandMineCell(selectedRowIndex, selectedColIndex)) {
-                BOARD[selectedRowIndex][selectedColIndex] = Cell.of(LAND_MINE_SIGN);
+                BOARD[selectedRowIndex][selectedColIndex] = Cell.ofLandMine();
                 changeGameStatusToLose();
                 return;
             }
@@ -132,7 +128,7 @@ public class MinesweeperGame {
     private static void initializeGame() {
         for (int row = 0; row < BOARD_ROW_SIZE; row++) {
             for (int col = 0; col < BOARD_COL_SIZE; col++) {
-                BOARD[row][col] = Cell.of(CLOSED_CELL_SIGN);
+                BOARD[row][col] = Cell.ofClosed();
             }
         }
 
@@ -228,24 +224,24 @@ public class MinesweeperGame {
     private static boolean isAllCellOpened() {
         return Arrays.stream(BOARD)
                 .flatMap(Arrays::stream)
-                .noneMatch(cell -> cell.equalsSign(CLOSED_CELL_SIGN));
+                .noneMatch(Cell::isClosed);
     }
 
     private static void open(int row, int col) {
         if (row < 0 || row >= BOARD_ROW_SIZE || col < 0 || col >= BOARD_COL_SIZE) {
             return;
         }
-        if (BOARD[row][col].doesNotEqualSign(CLOSED_CELL_SIGN)) {
+        if (BOARD[row][col].doesNotClosed()) {
             return;
         }
         if (isLandMineCell(row, col)) {
             return;
         }
         if (NEARBY_LAND_MINE_COUNT[row][col] != 0) {
-            BOARD[row][col] = Cell.of(String.valueOf(NEARBY_LAND_MINE_COUNT[row][col]));
+            BOARD[row][col] = Cell.ofNearByLandMineCount(NEARBY_LAND_MINE_COUNT[row][col]);
             return;
         } else {
-            BOARD[row][col] = Cell.of(OPENED_CELL_SIGN);
+            BOARD[row][col] = Cell.ofOpened();
         }
         open(row - 1, col - 1);
         open(row - 1, col);
--- src/main/java/cleancode/minesweeper/tobe/Cell.java
+++ src/main/java/cleancode/minesweeper/tobe/Cell.java
@@ -5,6 +5,11 @@ package cleancode.minesweeper.tobe;
  */
 public class Cell {
 
+    public static final String FLAG_SIGN = "⚑";
+    public static final String CLOSED_CELL_SIGN = "□";
+    public static final String LAND_MINE_SIGN = "☼";
+    public static final String OPENED_CELL_SIGN = "■";
+
     private final String sign;
 
     public Cell(String sign) {
@@ -13,6 +18,26 @@ public class Cell {
 
     public static Cell of(String sign) {
         return new Cell(sign);
+    }
+
+    public static Cell ofFlag() {
+        return of(FLAG_SIGN);
+    }
+
+    public static Cell ofLandMine() {
+        return of(LAND_MINE_SIGN);
+    }
+
+    public static Cell ofClosed() {
+        return of(CLOSED_CELL_SIGN);
+    }
+
+    public static Cell ofOpened() {
+        return of(OPENED_CELL_SIGN);
+    }
+
+    public static Cell ofNearByLandMineCount(int count) {
+        return of(String.valueOf(count));
     }
 
     public String getSign() {
@@ -25,5 +50,13 @@ public class Cell {
 
     public boolean doesNotEqualSign(String sign) {
         return !equalsSign(sign);
+    }
+
+    public boolean isClosed() {
+        return CLOSED_CELL_SIGN.equals(this.sign);
+    }
+
+    public boolean doesNotClosed() {
+        return !isClosed();
     }
 }
